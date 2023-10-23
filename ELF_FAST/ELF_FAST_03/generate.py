a9efcb785f7a4959ae9c677c37bf734dab52f70d
pierre <pierre1.dallara@epitech.eu>
2023-10-23 15:01:26 +0200

    [ADD] ELF_FAST_03 with ghidra and gdb stepi

--- ELF_FAST/ELF_FAST_03/generate.py
+++ ELF_FAST/ELF_FAST_03/generate.py
@@ -1,9 +1,11 @@
-stringsGet1 = "aI6yUVsaHg8FqvZE"
-offset = [2, 3, 2, 3, 5]
+# stringsGet1 = "aI6yUVsaHg8FqvZE"
+# stringsGet1 = "I6yUVsaHg8FqvZED"
+stringsGet1 = "aI6yUVsag8FqvZED"
+offset = [2, 3, 2, 3, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 password = ""
 
 for i in range(len(stringsGet1)):
-    password += chr(ord(stringsGet1[i]) - offset[i % 5])
+    password += chr(ord(stringsGet1[i]) + offset[i])
 
 print(f"Generated password: {password}")
 print(f"Generated password len: {len(password)}")
--- ELF_FAST/ELF_FAST_03/script_ELF_FAST_03.py
+++ ELF_FAST/ELF_FAST_03/script_ELF_FAST_03.py
@@ -3,11 +3,17 @@ import subprocess
 import os
 
 def generatePassword(part1, part2):
-    local_values = [2, 3, 2, 3, 5]
+    stringsGet = part1[0:8] + part2[0:8]
+    offset = [2, 3, 2, 3, 5]
     password = ""
-    for i in range(len(part1)):
-        password += chr(ord(part1[i]) + local_values[i])
-        if (i == len(local_values) - 1): break
+
+    for i in range(len(offset)):
+        password += chr(ord(stringsGet[i]) + offset[i])
+
+    password = password + part1[5:8] + part2[0:8]
+
+    # print(f"Generated password: {password}")
+    # print(f"Generated password len: {len(password)}")
     return password
 
 
@@ -17,12 +23,12 @@ def check_password(password, binary_path="./ELF_03_downloaded"):
         return True
     return False
 
-# # Étape 1: Téléchargez le fichier
-# url = "http://reverse.blackfoot.io:8080/ELF_03"
-# response = requests.get(url)
-# with open("ELF_03_downloaded", "wb") as file:
-#     file.write(response.content)
-# os.chmod("ELF_03_downloaded", 0o755)
+# Étape 1: Téléchargez le fichier
+url = "http://reverse.blackfoot.io:8080/ELF_03"
+response = requests.get(url)
+with open("ELF_03_downloaded", "wb") as file:
+    file.write(response.content)
+os.chmod("ELF_03_downloaded", 0o755)
 
 # Étape 2: Exécutez `strings` sur le fichier téléchargé
 result = subprocess.run(["strings", "ELF_03_downloaded"], capture_output=True, text=True)
@@ -33,20 +39,20 @@ part2 = lines[13]
 if len(part1) != len("g5Eeil7QH"):
     part1 = lines[13]
     part2 = lines[14]
-print(part1)
-print(part2)
+# print(part1)
+# print(part2)
 
 password = generatePassword(part1, part2)
 # Étape 3: Vérifiez le mot de passe avec le binaire
 if check_password(password):
     print(f"Password found: {password}")
 
-    # # Étape 4: Faites une requête HTTP pour valider le flag
-    # validate_url = f"http://reverse.blackfoot.io:8080/validate/ELF_03/{password}"
-    # response = requests.get(validate_url)
+    # Étape 4: Faites une requête HTTP pour valider le flag
+    validate_url = f"http://reverse.blackfoot.io:8080/validate/ELF_03/{password}"
+    response = requests.get(validate_url)
 
-    # # Étape 5: Vérifiez la réponse
-    # print(response.text)
+    # Étape 5: Vérifiez la réponse
+    print(response.text)
     exit(0)
 
 print("Failed to find the correct password.")
