b04ec23bfe940864286fe474f48cfb40a8c3163a
pierre <pierre1.dallara@epitech.eu>
2023-10-27 21:04:37 +0200

    [ADD] Last push

--- Obfu/bruteForce.py
+++ Obfu/bruteForce.py
@@ -1,22 +1,35 @@
-def decode_function(key, data):
-    decoded = []
-    key_len = len(key)
-    for i in range(len(data)):
-        decoded_char = chr(data[i] ^ ord(key[i % key_len]))
-        decoded.append(decoded_char)
-    return "".join(decoded)
+import string
+import subprocess
 
-def is_printable_ascii(s):
-    return all(32 <= ord(c) < 127 for c in s)
+# Les valeurs de DAT_00012008 que vous avez trouvées dans GDB
+data = [0x2f, 0xb9, 0x87, 0xc2, 0xfe, 0x98, 0x95, 0x8e, 0xeb, 0x96, 0x6d, 0xa1]
 
-def brute_force_key(data):
-    for i in range(32, 127):  # ASCII printable characters
-        for j in range(32, 127):
-            for k in range(32, 127):
-                key = chr(i) + chr(j) + chr(k)
-                decoded_data = decode_function(key, data)
-                if is_printable_ascii(decoded_data):
-                    print(f"Potential Key: {key}, Decoded Data: {decoded_data}")
+# Tous les caractères ASCII imprimables que vous souhaitez tester
+chars = string.ascii_letters + string.digits  # Lettres majuscules et minuscules + chiffres
 
-data = [0x2F, 0xB9, 0x87, 0xC2, 0xFE, 0x98, 0x95, 0x8E, 0xEB, 0x96, 0x6D, 0xA1]
-brute_force_key(data)
+def is_printable(s):
+    """Vérifie si tous les caractères de la chaîne sont imprimables"""
+    return all(c in string.printable for c in s)
+
+def test_password(password):
+    """Exécutez le binaire avec le mot de passe et vérifiez s'il renvoie 'Yes'"""
+    try:
+        result = subprocess.run(['./simple_binary', password], capture_output=True, text=True, timeout=2)
+        return "Yes" in result.stdout
+    except subprocess.TimeoutExpired:
+        print(f"Timeout for password: {password}")
+        return False
+    except Exception as e:
+        return False
+
+
+# Testez toutes les combinaisons possibles de 3 caractères
+charsK = chars[len('abcdefghij'):len(chars)]
+for c1 in charsK:
+    for c2 in chars:
+        for c3 in chars:
+            password = c1 + c2 + c3
+            print(f"Test: {password}")
+            if test_password(password):
+                print(f"Flag found: {password}")
+                exit(0)  # Arrête le script s'il trouve le bon mot de passe
